164cc2a849443d84349d79d55732a8bc6f64f01e
dangolrohit <rohit.god44455@gmail.com>
2023-07-21 17:22:19 +0545

    new formula to take % added.

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
-function calculateLove() {
-    var yourName = document.getElementById("yourName").value;
-    var partnerName = document.getElementById("partnerName").value;
-    var yourGender = document.querySelector('input[name="yourGender"]:checked');
-    var partnerGender = document.querySelector('input[name="partnerGender"]:checked');
+function calculateLovePercentage() {
+    const yourName = document.getElementById("yourName").value.toUpperCase().replace(/[^A-Z]/g, "");
+    const partnerName = document.getElementById("partnerName").value.toUpperCase().replace(/[^A-Z]/g, "");
 
-    if (!yourName.trim() || !partnerName.trim()) {
-        alert("Please enter both your names.");
-        return;
-    }
+    const countLetters = (name) => {
+        const letterCounts = {};
+        const letters = name.split("");
 
-    if (!yourGender || !partnerGender) {
-        alert("Please select both your genders.");
-        return;
-    }
+        letters.forEach((letter) => {
+            letterCounts[letter] = letterCounts[letter] ? letterCounts[letter] + 1 : 1;
+        });
 
-    var lovePercentage = Math.floor(Math.random() * 101); // Random number between 0 and 100 (inclusive)
+        return Object.values(letterCounts).join("");
+    };
 
-    document.getElementById("display").value = lovePercentage + "%";
+    const shortenNumber = (number) => {
+        const result = [];
+        const length = number.length;
+        for (let i = 0; i < Math.ceil(length / 2); i++) {
+            const left = parseInt(number[i]);
+            const right = parseInt(number[length - 1 - i]);
+            result.push(left + right);
+        }
+        return result.join("");
+    };
+
+    const calculateFinalResult = (number) => {
+        while (number.length > 2) {
+            number = shortenNumber(number);
+        }
+        return number;
+    };
+
+    const yourNameCount = countLetters(yourName);
+    const partnerNameCount = countLetters(partnerName);
+
+    const combinedCounts = yourNameCount + partnerNameCount;
+    const finalResult = calculateFinalResult(combinedCounts);
+
+    return finalResult;
 }
 
-var btnElement = document.getElementById("btn");
-var clickCount = 0;
-
-btnElement.addEventListener("click", function() {
-    clickCount++;
-    if (clickCount === 2) {
-        location.reload(); // Reload the page on double-click
-    }
-    setTimeout(function() {
-        clickCount = 0; // Reset clickCount after 1 second
-    }, 1000);
-});
+function displayLovePercentage() {
+    const lovePercentage = calculateLovePercentage();
+    document.getElementById("display").value = lovePercentage + "%";
+}
